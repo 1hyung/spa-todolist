9701873becbed932f60c0c081b6e01df51320619
1hyung <ryu1hyung@naver.com>
2024-06-07 16:59:56 +0900

    Initial commit

---
+++ src/main/kotlin/com/teamsparta/spatodolist/SpaTodolistApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.spatodolist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpaTodolistApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpaTodolistApplication>(*args)
+}
